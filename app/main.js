bc1ac45763c679103cec08ab24e430a6fcdf0721
Raman Sharma <rs9463194297@gmail.com>
2023-03-12 16:36:01 +0530

    added ping command support

--- app/main.js
+++ app/main.js
@@ -1,11 +1,14 @@
 const net = require("net");
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-// Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-  // Handle connection
+  connection.on("data", (data) => {
+    const commands = data.toString().trim().split("\r\n");
+    console.log(commands);
+    if (commands[2].toUpperCase() === "PING") {
+      connection.write("+PONG\r\n");
+    }
+  });
 });
-//
 server.listen(6379, "127.0.0.1");

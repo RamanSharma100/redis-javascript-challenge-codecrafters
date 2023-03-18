35bfffa1eca738cf4c8eacbf899102f4afeea801
Raman Sharma <rs9463194297@gmail.com>
2023-03-18 15:47:14 +0530

    added get and set without modes

--- app/main.js
+++ app/main.js
@@ -2,6 +2,13 @@ const net = require("net");
 
 console.log("Logs from your program will appear here!!");
 
+const TOTAL_DBS = 20;
+const DEFAULT_DB = 0;
+const MAX_KEY_SIZE = 512;
+const MAX_VALUE_SIZE = 1024 * 1024 * 1024;
+
+const storage = Array.from({ length: TOTAL_DBS }, () => new Map());
+
 const server = net.createServer((connection) => {
   connection.on("data", (data) => {
     const commands = data.toString().trim().split("\r\n");
@@ -13,6 +20,33 @@ const server = net.createServer((connection) => {
 
     if (commands[2].toUpperCase() === "ECHO") {
       connection.write(`$${commands[4].length}\r\n${commands[4]}\r\n`);
+    }
+
+    if (commands[2].toUpperCase() === "SET") {
+      if (commands[4].length > MAX_KEY_SIZE) {
+        connection.write("-ERR key is too long\r\n");
+      }
+
+      if (commands[6].length > MAX_VALUE_SIZE) {
+        connection.write("-ERR value is too long\r\n");
+      }
+
+      storage[DEFAULT_DB].set(commands[4], commands[6]);
+      console.log(storage);
+      connection.write("+OK\r\n");
+    }
+
+    if (commands[2].toUpperCase() === "GET") {
+      if (storage[DEFAULT_DB].has(commands[4])) {
+        storage[DEFAULT_DB].get(commands[4]);
+        connection.write(
+          `$${storage[DEFAULT_DB].get(commands[4]).length}\r\n${storage[
+            DEFAULT_DB
+          ].get(commands[4])}\r\n`
+        );
+      } else {
+        connection.write("-ERR no such key\r\n");
+      }
     }
   });
 });
